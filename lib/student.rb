5db217d626ae8aafbd1eb190a76e02431ad69948
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-10-20 20:21:54 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
-
+class Student < User 
+  def initialize
+    @knowledge = []
+  end 
+  def learn(string_given)
+    @knowledge << string_given
+  end
+  def knowledge 
+    @knowledge
+  end 
 end
